eb8b31c92baa56deb7c241049a9f726b5fbbedb4
Willy Ontech <n.willy@alustudent.com>
2025-07-10 07:47:12 -0700

    Fix PEP8 issues in 0-safe_print_list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,8 @@
-def safe_print_list(my_list=[], x=0):
+#!/usr/bin/python3
+def safe_print_list(my_list=None, x=0):
+    if my_list is None:
+        my_list = []
+
     count = 0
     try:
         while count < x:
@@ -7,4 +11,4 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         pass
     print()
-    return count
+   return count
