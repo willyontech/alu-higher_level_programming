eb4dd14e13f0088be4f08973707a3646af397660
Willy Ontech <n.willy@alustudent.com>
2025-07-13 02:38:27 -0700

    Add 4-list_division.py for element-wise list division with exceptions

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+
+    for i in range(list_length):
+        try:
+            a = my_list_1[i]
+            b = my_list_2[i]
+            if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+                print("wrong type")
+                res = 0
+            else:
+                res = a / b
+        except ZeroDivisionError:
+            print("division by 0")
+            res = 0
+        except IndexError:
+            print("out of range")
+            res = 0
+        finally:
+            result.append(res)
+
+    return result
