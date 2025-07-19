80c761fe090146f537c72c0fcfe9d8a3dbaeaf79
Willy Ontech <n.willy@alustudent.com>
2025-07-19 05:23:59 -0700

    Add PEP8 clean 101-square.py

---
+++ python-classes/101-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize the square with size and position."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position with validation."""
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) and num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integer")
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with '#' respecting position."""
+        if self.__size == 0:
+            print()
+            return
+
+        [print() for _ in range(self.__position[1])]
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        """Define string representation for printing with `print()`."""
+        if self.__size == 0:
+            return ""
+
+        result = "\n" * self.__position[1]
+        for i in range(self.__size):
+            result += " " * self.__position[0] + "#" * self.__size
+            if i != self.__size - 1:
+                result += "\n"
+        return result
